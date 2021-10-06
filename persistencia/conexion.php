cef9c840817e7cca7d53fd29cd62a94209345e93
D4ni3lBr4u3r <73198107+D4ni3lBr4u3r@users.noreply.github.com>
2021-10-05 23:26:53 -0500

    Archivos phpStorm, comentarios XD

--- persistencia/conexion.php
+++ persistencia/conexion.php
@@ -1,5 +1,9 @@
 <?php
     class Conexion{
+        /**
+         * Metodo que permite la conexion de una base de datos PostgreSQL local
+         * @return la conexion
+         */
         public function conectarDB(){
             $dbhost = "localhost";
             $dbuser = "postgres";
@@ -12,6 +16,11 @@
             return $conn;
         }
 
+        /**
+         * Metodo que permite la desconexion de una base de datos PostgreSQL
+         * @param $conexion la conexion que se encuentra abierta
+         * @return el cierre de la conexion
+         */
         public function desconectarDB($conexion){
             $cerrar = pg_close($conexion)
             or die("Ha sucedido un erro inesperado en la conexion de la base de datos");
