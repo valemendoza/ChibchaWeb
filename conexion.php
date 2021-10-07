81d8d71516317e35c08b753eccecdc58580db85c
Vale Mendoza <vmendoza@unbosque.edu.co>
2021-10-07 14:27:19 -0500

    prueba sql V

--- conexion.php
+++ conexion.php
@@ -5,8 +5,8 @@ $nombreBaseDeDatos = "mblffhll";
 $rutaServidor = "fanny.db.elephantsql.com";
 $puerto = "5432";
 try {
-    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
-    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bd = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
+    $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo("SIII");
 } catch (Exception $e) {
     echo "Ocurrió un error con la base de datos: " . $e->getMessage();
