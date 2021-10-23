ca4b7457c3cce192794f9bc783e132f214977514
Vale Mendoza <vmendoza@unbosque.edu.co>
2021-10-23 16:28:16 -0500

    cambios

--- logica/paqueteE.php
+++ logica/paqueteE.php
@@ -20,5 +20,5 @@ echo ("El plan escogido es el numero: ".$plan);
 $query = $bd->prepare('UPDATE "Cliente" SET "Tipo_Plan_Id_Tipo_Plan" ='.$plan.', "Tipo_Paquete_Id_Tipo_Paquete" ='.$paquete.' WHERE "Correo" =:sesion ;');
 $query->bindParam(":sesion", $usuarioActual);
 $query->execute();
-echo "hecho";
-header("location:../presentacion/inicioCliente.php");
+
+header("location:https://merchants.payulatam.com/login/auth?__hstc=167585569.39015f6272334b606465ea9e312a8433.1633312631759.1633312631759.1635024034941.2&__hssc=167585569.1.1635024034941&__hsfp=1908706972");
