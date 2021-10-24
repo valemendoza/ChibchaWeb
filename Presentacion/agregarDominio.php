44933e6601f1ef1fcc6d64baddbfd14d5c916548
Vale Mendoza <vmendoza@unbosque.edu.co>
2021-10-23 19:50:31 -0500

    ah bueno

--- Presentacion/agregarDominio.php
+++ Presentacion/agregarDominio.php
@@ -169,9 +169,7 @@ if(isset($_POST['btcerrarS'])){
                 $distribuidorPartes=explode(',', $distribuidor);
                 $extension=$distribuidorPartes[1];
                 $id=$distribuidorPartes[0];
-                echo "<script>
-                alert('Aqui si funciona.');
-                </script>";
+               
                 
                 $pagina = 'http://www.'.$buscaweb.$extension;
                 if (@fopen($pagina,»r»)){
